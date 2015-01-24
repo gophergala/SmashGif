7b9a909d16b5be0e183c6cae57310de7c624203e
Andy Zhang <andzhng@gmail.com>
2015-01-23 22:04:29 -0500

    Use Martini as web framework, added middleware

--- src/hello.go
+++ src/hello.go
@@ -1,14 +1,22 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
-}
+	m := martini.Classic()
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Hello, world!")
+	// Middle ware stuff
+	m.Use(render.Renderer())
+	m.Use(martini.Static("public"))
+
+	// Request handlers
+	m.Get("/", func(r render.Render) {
+		r.HTML(200, "index", nil)
+	})
+
+	http.Handle("/", m)
 }
