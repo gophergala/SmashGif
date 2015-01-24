6a6ba8d9e493759670fdee7ea8876f8a04df138a
Andy Zhang <andzhng@gmail.com>
2015-01-23 22:39:50 -0500

    Fixed static file requests

--- src/hello.go
+++ src/hello.go
@@ -8,15 +8,21 @@ import (
 
 func init() {
 	m := martini.Classic()
-
 	// Middle ware stuff
-	m.Use(render.Renderer())
+	m.Use(render.Renderer(render.Options{
+		Directory: "public",
+	}))
+	m.Use(martini.Logger())
 	m.Use(martini.Static("public"))
 
 	// Request handlers
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", nil)
+		r.HTML(200, "main", nil)
 	})
 
 	http.Handle("/", m)
+	// m.Run()
+}
+
+func main() {
 }
